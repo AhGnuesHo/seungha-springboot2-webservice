5c7fbf85518b70e1f03c7d9f348ec16921954f75
seungha <frazzle124@gmail.com>
2022-02-03 21:34:39 +0900

    구글 OAuth 및 스프링 시큐리티 설정

---
+++ src/main/java/com/seungha/study/cofig/oauth/dto/SessionUser.java
@@ -0,0 +1,20 @@
+package com.seungha.study.cofig.oauth.dto;
+
+import com.seungha.study.domain.user.User;
+import lombok.Getter;
+
+import java.io.Serializable;
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+
+    public SessionUser(User user) {
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+    }
+
+}
---
+++ src/main/java/com/seungha/study/domain/user/UserRepostiory.java
@@ -0,0 +1,9 @@
+package com.seungha.study.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepostiory extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
